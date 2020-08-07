de8e186c0191477fb2101101ec6dd6d50a8e8f77
Yogesh Kumar Singh <s.ashucool14@gmail.com>
2020-08-08 00:19:41 +0530

    initial commit - song upload functionality added

---
+++ app/src/main/java/com/suno/MainActivity.java
@@ -0,0 +1,162 @@
+package com.suno;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.OpenableColumns;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageException;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.net.URI;
+import java.util.Objects;
+import java.util.UUID;
+import java.util.logging.Logger;
+
+public class MainActivity extends AppCompatActivity {
+    private boolean checkPermission = false;
+    Uri uri;
+    private StorageReference storageReference;
+    ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        storageReference = FirebaseStorage.getInstance().getReference();
+        progressDialog = new ProgressDialog(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.app_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.uploadItem){
+            if (validatePermissions()){
+                pickSong();
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void pickSong() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("audio/*");
+        startActivityForResult(intent,1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (data != null) {
+            if (requestCode == 1 && resultCode == RESULT_OK) {
+                uri = data.getData();
+                Log.i("uri", uri.toString());
+                String fileName = getFileName(uri);
+                Log.i("songName", fileName);
+                uploadFileToServer(uri,fileName);
+            }
+        }
+    }
+
+    // METHOD TO HANDEL SONG UPLOAD TO THE STORAGE SERVER
+    public void uploadFileToServer(Uri uri, String songName){
+        StorageReference filePath = storageReference.child("Audios").child(songName);
+        progressDialog.show();
+        filePath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Log.i("success", "upload");
+                progressDialog.dismiss();
+                Toast.makeText(MainActivity.this, "Upload Successful", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
+                double progress = (100.0*taskSnapshot.getBytesTransferred())/taskSnapshot.getTotalByteCount();
+                int currentProgress = (int) progress;
+                progressDialog.setMessage("Uploading: " + currentProgress + "%");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.i("success", "upload");
+                progressDialog.dismiss();
+                Toast.makeText(MainActivity.this, "Upload Failed! Please Try again!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    // METHOD TO GET THE SONG NAME
+    public String getFileName(Uri uri) {
+        String result = null;
+        if (Objects.equals(uri.getScheme(), "content")) {
+            try (Cursor cursor = getContentResolver().query(uri, null, null, null, null)) {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                }
+            }
+        }
+        if (result == null) {
+            result = uri.getPath();
+            assert result != null;
+            int cut = result.lastIndexOf('/');
+            if (cut != -1) {
+                result = result.substring(cut + 1);
+            }
+        }
+        return result;
+    }
+
+    // METHOD TO HANDEL RUNTIME PERMISSIONS
+    private boolean validatePermissions(){
+        Dexter.withContext(getApplicationContext())
+                .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                        checkPermission = true;
+                        pickSong();
+                    }
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                        checkPermission = false;
+                    }
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                        permissionToken.continuePermissionRequest();
+                    }
+                }).check();
+        return checkPermission;
+    }
+}
