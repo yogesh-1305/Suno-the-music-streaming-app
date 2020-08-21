1b3ef4e16d02233f115a58279734d942041343d1
Yogesh Kumar Singh <s.ashucool14@gmail.com>
2020-08-21 19:32:59 +0530

    added splash screen

---
+++ app/src/main/java/com/suno/SplashScreen.java
@@ -0,0 +1,18 @@
+package com.suno;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        startActivity(new Intent(this, SignUpActivity.class));
+        finish();
+
+    }
+}
